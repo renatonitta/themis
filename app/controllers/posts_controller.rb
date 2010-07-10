5c3094478475f1eb38c22ebcf9c470b92606bbf4
Diego Carrion <dc.rec1@gmail.com>
2010-07-10 03:16:59 -0300

    a post is created with the logged user as the owner

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,10 @@
 class PostsController < InheritedResources::Base
+  before_filter :authenticate_user!, :only => [:create]
+  before_filter :assign_user
+
+  private
+
+  def assign_user
+    params[:post][:user] = current_user
+  end
 end
