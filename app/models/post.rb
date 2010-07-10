02c7c83d2635aca2f8d84df7a5f718c60099436e
Diego Carrion <dc.rec1@gmail.com>
2010-07-10 03:54:50 -0300

    post has title as an slugged friendly id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :title, :body, :user
+
+  has_friendly_id :title, :use_slug => true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,4 +3,9 @@ require 'spec_helper'
 describe Post do
   should_belong_to :user
   should_validate_presence_of :title, :body, :user
+
+  it "should be found by the slugged title as an id" do
+    post = Factory(:post, :title => "Hello World")
+    Post.find(post.id).should == Post.find("hello-world")
+  end
 end
