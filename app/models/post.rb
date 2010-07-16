0449d6432d660ce5b1d2e4015af78698c235d9ef
Diego Carrion <dc.rec1@gmail.com>
2010-07-16 19:04:46 -0300

    a post belongs to a section

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  belongs_to :user
   belongs_to :author, :class_name => 'User', :foreign_key => "user_id"
+  belongs_to :section
   validates_presence_of :title, :body, :user
 
   has_friendly_id :title, :use_slug => true
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Post do
-  should_belong_to :user
   should_belong_to :author, :class_name => "User", :foreign_key => "user_id"
+  should_belong_to :section
   should_validate_presence_of :title, :body, :user
 
   it "should be found by the slugged title as an id" do
