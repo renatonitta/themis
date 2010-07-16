68e6a9fd4f7532eabb61d989922f5f5a555f618a
Diego Carrion <dc.rec1@gmail.com>
2010-07-16 16:38:26 -0300

    user can be created with a name

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
 end
