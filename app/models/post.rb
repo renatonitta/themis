c3edb28bd1fe13b159a6b6e282b5d644ff8ca5ba
Ricardo Bernardelli <ricardobcs@gmail.com>
2010-07-19 17:29:39 -0300

    added author instead user for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  belongs_to :author, :class_name => 'User', :foreign_key => "user_id"
   validates_presence_of :title, :body, :user
 
   has_friendly_id :title, :use_slug => true
