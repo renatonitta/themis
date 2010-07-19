bf5cc32a612d9eb7027e1f40e2f374aedd9d339b
BrunoGrasselli <bruno.grasselli@gmail.com>
2010-07-19 19:41:46 -0300

    Using domain's language on search feature

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -8,3 +8,11 @@ end
 Then /^I should see a form with action "([^"]*)"$/ do |action|
   locate("form[action='#{action}']")
 end
+
+Then /^I should see the search form$/ do
+  Then %{I should see a form with action "http://google.com/search"}
+  Then %{I should see an input type "hidden" with name "sitesearch" and value "www.example.com"}
+  Then %{I should see an input type "text" with name "q" and value ""}
+  Then %{I should see an input type "submit" with name "commit" and value "Search"}
+end
+
