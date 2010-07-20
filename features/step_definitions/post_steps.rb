9802650c2de24c3cede379ba44c4c6e5927d7c1b
BrunoGrasselli <bruno.grasselli@gmail.com>
2010-07-20 16:20:05 -0300

    Adding post tags as html meta tags

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -2,3 +2,8 @@ When /^I visit new post$/ do
   visit new_section_post_path(Section.last)
 end
 
+Then /^I should see the "([^\"]*)" tags as html meta tags$/ do |title|
+  post = Post.find_by_title(title)
+  locate("meta[name='keywords'][content='#{post.tag_list}']")
+end
+
