7293bd6aeb31fa39236b3c994774cbe95ffa8a3a
Diego Carrion <dc.rec1@gmail.com>
2010-07-20 21:07:12 -0300

    /sections/:section/posts/:post/approve targets posts_controller#approve

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Themis::Application.routes.draw do |map|
   resources :sections do
-    resources :posts
+    resources :posts do
+      member do
+        match 'approve' => 'posts#approve', :as => 'approve'
+      end
+    end
   end
   devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout' }
   match 'tags/:tag', :to => 'posts#by_tag', :as => 'tags'
