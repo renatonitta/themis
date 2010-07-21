54f5c6fd48669cf138e02957f15b0d1d5321454b
Diego Carrion <dc.rec1@gmail.com>
2010-07-21 12:55:36 -0300

    only a logged admin can post to users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe UsersController do
   include Devise::TestHelpers
 
-  context "with a logged Admin" do
+  context "with a logged admin" do
     before :each do
       sign_in Factory(:admin)
     end
@@ -12,6 +12,19 @@ describe UsersController do
       it "should create an user" do
         post :create, :user => { :name => "Mike Tyson", :email => "mike@tyson.com", :password => "mike123" }   
         User.find_by_email("mike@tyson.com").should_not be_nil
+      end
+    end
+  end
+
+  context "with a logged iuser" do
+    before :each do
+      sign_in Factory(:user)
+    end
+
+    describe "responding to POST create" do
+      it "should return 306 as the status code" do
+        post :create, :user => { :name => "Mike Tyson", :email => "mike@tyson.com", :password => "mike123" }   
+        response.code.should eql("302")
       end
     end
   end
