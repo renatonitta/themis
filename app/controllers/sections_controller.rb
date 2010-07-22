b7d8d86d0a1d578950fc623cd881c2e09402b3c0
Diego Carrion <dc.rec1@gmail.com>
2010-07-22 18:41:17 -0300

    only admins can manage sections

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,3 +1,3 @@
 class SectionsController < InheritedResources::Base
-  before_filter :authenticate_user!, :only => [:create, :update, :destroy]
+  before_filter :authenticate_admin!, :only => [:create, :update, :destroy]
 end
--- spec/controllers/sections_controller_spec.rb
+++ spec/controllers/sections_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe SectionsController do
-  should_require_authentication_on_private_actions
+  should_require_admin_authentication_on_private_actions
 end
