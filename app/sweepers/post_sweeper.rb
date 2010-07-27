01942edbc8b06e2b70a35fc9c41a448edf0a44fb
Ricardo Bernardelli <ricardobcs@gmail.com>
2010-07-27 10:57:11 -0300

    refactoring

--- app/sweepers/post_sweeper.rb
+++ app/sweepers/post_sweeper.rb
@@ -13,9 +13,15 @@ class PostSweeper < ActionController::Caching::Sweeper
   
   def expire_cache(post)
     expire_page root_path
-    (Post.count / Post::PER_PAGE.to_f).ceil.times { |i| expire_page posts_page_path(i + 1) }
     expire_page section_post_path(post.section, post)
     expire_page section_posts_path(post.section)
-    (post.section.posts.count / Post::PER_PAGE.to_f).ceil.times { |i| expire_page page_section_posts_path(post.section.id, i + 1) }
+    pages_count(Post.count).times { |i| expire_page posts_page_path(i + 1) }
+    pages_count(post.section.posts.count).times { |i| expire_page page_section_posts_path(post.section.id, i + 1) }
+  end
+
+  private
+
+  def pages_count(posts_size)
+    (posts_size / Post::PER_PAGE.to_f).ceil
   end
 end
