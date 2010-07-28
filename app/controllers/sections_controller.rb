ff5603d13309bd969722a218605f75d033612de2
Diego Carrion <dc.rec1@gmail.com>
2010-07-28 20:36:23 -0300

    removed code that doesn't break tests, aka refactoring

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,7 +1,6 @@
 class SectionsController < InheritedResources::Base
   before_filter :authenticate_admin!, :only => [:create, :update, :destroy, :edit, :new]
-  respond_to :js, :only => :destroy
-
+  respond_to :js
   def create
     create! do |success, failure|
       success.html { redirect_to admin_sections_path }
