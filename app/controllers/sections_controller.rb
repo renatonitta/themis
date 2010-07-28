a029de6192e437b6fa527dab738390413ed8d33a
Diego Carrion <dc.rec1@gmail.com>
2010-07-28 20:41:00 -0300

    removed more code that doesn't break tests

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,14 +2,10 @@ class SectionsController < InheritedResources::Base
   before_filter :authenticate_admin!, :only => [:create, :update, :destroy, :edit, :new]
   respond_to :js
   def create
-    create! do |success, failure|
-      success.html { redirect_to admin_sections_path }
-    end
+    create! { redirect_to admin_sections_path }
   end
 
   def update
-    update! do |success, failure|
-      success.html { redirect_to admin_sections_path }
-    end
+    update! { redirect_to admin_sections_path }
   end
 end
