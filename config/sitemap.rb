9cf1010588c2f418c30b3718c31e739bdf20dd03
Ricardo Bernardelli <ricardobcs@gmail.com>
2010-07-30 12:39:06 -0300

    Host config added

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://blog.gonow.com.br"
+SitemapGenerator::Sitemap.default_host = Themis::Config["host"]
 
 def pages_count(posts_size)
   (posts_size / Post::PER_PAGE.to_f).ceil
@@ -15,11 +15,13 @@ SitemapGenerator::Sitemap.add_links do |sitemap|
   pages_count(Post.count).times { |i| sitemap.add posts_page_path(i + 1) }
 
   Post.approved.all.each do |post|
-    sitemap.add section_post_path(post.section, post), :lastmod => post.updated_at
+    sitemap.add section_post_path(post.section, post)
   end
 
   Section.all.each do |section|
-    sitemap.add section_posts_path(section), :lastmod => section.posts.approved.first.updated_at
+    sitemap.add section_posts_path(section)
     pages_count(section.posts.count).times { |i| sitemap.add page_section_posts_path(section, i + 1) }
   end
+
+  File.open("public/robots.txt", 'w') {|f| f.write("Sitemap: #{SitemapGenerator::Sitemap.default_host}/sitemap1.xml.gz") }
 end
