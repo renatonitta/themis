99902b185080fe614b950c0e5609ede0db617531
Diego Carrion <dc.rec1@gmail.com>
2010-08-03 00:31:14 -0300

    application_controller#sitemap can render the default view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
-  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
-    render :text => exception, :status => 500
-  end
   protect_from_forgery
   layout 'application'
 
@@ -10,9 +7,5 @@ class ApplicationController < ActionController::Base
   def sitemap
     @posts = Post.approved.all
     @sections = Section.all
-
-    respond_to do |format|
-      format.xml { render :template => 'shared/sitemap' }
-    end
   end
 end
