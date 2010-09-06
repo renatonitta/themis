c6530348b7c660eb66a8a29cb7e7b2f4800635cc
BrunoGrasselli <bruno.grasselli@gmail.com>
2010-09-06 18:36:38 -0300

    As AdminUsers#show is a blank page, update and create should redirect to index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+  include InheritedResources::DSL
 
   caches_page :sitemap
 
--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -2,4 +2,24 @@ require 'spec_helper'
 
 describe Admin::UsersController do
   should_require_admin_authentication
+
+  context "logged admin" do
+    before :each do
+      controller.stub!(:authenticate_admin!)
+    end
+
+    describe "POST create" do
+      it "should redirect to index" do
+        post :create, :user => {:name => 'Test', :email => 'test@mail.com', :password => '123456', :password_confirmation => '123456'}
+        response.should redirect_to(admin_users_path)
+      end
+    end
+
+    describe "PUT update" do
+      it "should redirect to index" do
+        put :update, :id => Factory(:user).id
+        response.should redirect_to(admin_users_path)
+      end
+    end
+  end
 end
