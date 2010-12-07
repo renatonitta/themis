3446895dd54d0e313eb9696227237ae7dec71691
Diego Carrion <dc.rec1@gmail.com>
2010-12-07 15:23:36 -0200

    updated matchers

--- spec/support/controllers.rb
+++ spec/support/controllers.rb
@@ -1,11 +1,3 @@
-def should_have_an_slugged_id(name)
-  it "should be found by the slugged #{name} as an id" do
-    clazz = subject.class
-    object = Factory(clazz.to_s.underscore, name => "Hello World")
-    clazz.find(object.id).should == clazz.find("hello-world")
-  end
-end
-
 def should_require_authentication_on_private_actions
   include Devise::TestHelpers
 
@@ -17,9 +9,23 @@ def should_require_authentication_on_private_actions
       end
     end
 
+    describe "GET new" do
+      it "should return 302 as the status code"  do
+        get :new
+        response.code.should eql("302")
+      end
+    end
+
     describe "POST create" do
       it "should return 302 as the status code"  do
         post :create
+        response.code.should eql("302")
+      end
+    end
+
+    describe "GET edit" do
+      it "should return 302 as the status code"  do
+        get :edit, :id => 30
         response.code.should eql("302")
       end
     end
